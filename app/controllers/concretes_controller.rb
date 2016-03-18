6b568888454f98d88e591cacafd959a51b51fbfd
psswf1172 <jennifermabella@gmail.com>
2016-03-18 19:27:38 -0400

    done

--- app/controllers/concretes_controller.rb
+++ app/controllers/concretes_controller.rb
@@ -4,6 +4,7 @@ class ConcretesController < ApplicationController
   end
 
   def create
+    @concrete = Concrete.create(concrete_params)
     render nothing: true
   end
 
@@ -14,4 +15,10 @@ class ConcretesController < ApplicationController
   def update
     render nothing: true
   end
+
+  private
+  def concrete_params
+    params.require(:concrete).permit(:mix_type, :color, :psi, :cost_per_yard)
+  end
+
 end
